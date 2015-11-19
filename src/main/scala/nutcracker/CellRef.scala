e09375b0bd15e33992f6ee5697fac76529c7f8ea
Tomas Mikula <tomas.mikula@gmail.com>
2015-11-19 17:24:00 -0500

    Add ==> and <=> propagators.

--- src/main/scala/nutcracker/CellRef.scala
+++ src/main/scala/nutcracker/CellRef.scala
@@ -1,4 +1,9 @@
 package nutcracker
 
+import nutcracker.ProblemDescription.{Second, Intersect, VarTrigger}
+
 sealed trait CellRef[D]
-case class PureDomRef[+A, D] private[nutcracker](private[nutcracker] val domainId: Long) extends CellRef[D]
+case class PureDomRef[+A, D] private[nutcracker](private[nutcracker] val domainId: Long) extends CellRef[D] {
+  def ==>(target: PureDomRef[_, D]): ProblemDescription[Unit] = VarTrigger(this, (d: D) => Intersect(target, d))
+  def <=>(target: PureDomRef[_, D]): ProblemDescription[Unit] = Second(this ==> target, target ==> this)
+}
