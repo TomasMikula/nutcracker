f36129834ab2514a1726530f3b7f5b8101768223
Tomas Mikula <tomas.mikula@gmail.com>
2015-12-18 14:39:59 -0500

    Supply FunctorKA instance for BranchLang.

--- src/main/scala/nutcracker/BranchLang.scala
+++ src/main/scala/nutcracker/BranchLang.scala
@@ -2,7 +2,8 @@ package nutcracker
 
 import scala.language.higherKinds
 
-import nutcracker.util.free.{InjectK, FreeK}
+import nutcracker.util.free.{FunctorKA, InjectK, FreeK}
+import scalaz.{Functor, ~>}
 
 sealed trait BranchLang[B[_], K[_], A]
 
@@ -13,4 +14,12 @@ object BranchLang {
 
   def addBranchingF[B[_], F[_[_], _]](b: B[FreeK[F, Unit]])(implicit inj: InjectK[BranchLang[B, ?[_], ?], F]): FreeK[F, Unit] =
     FreeK.lift[BranchLang[B, ?[_], ?], F, Unit](addBranching[B, FreeK[F, ?]](b))
+
+  implicit def functorKInstance[B[_]: Functor]: FunctorKA[BranchLang[B, ?[_], ?]] = new FunctorKA[BranchLang[B, ?[_], ?]] {
+
+    def transform[K[_], L[_], A](bk: BranchLang[B, K, A])(tr: K ~> L): BranchLang[B, L, A] = bk match {
+      case AddBranching(b) => AddBranching[B, L](Functor[B].map(b)(k => tr(k)))
+    }
+
+  }
 }
