7b952e4161155fb65d500d2eb04d64c550c98069
Tomas Mikula <tomas.mikula@gmail.com>
2016-02-08 22:29:03 -0500

    Support pattern matching with partially assigned vertex values.

--- src/main/scala/nutcracker/rel/RelDB.scala
+++ src/main/scala/nutcracker/rel/RelDB.scala
@@ -13,11 +13,13 @@ import scalaz.{Foldable, Applicative}
 import scalaz.std.list._
 import shapeless.HList
 
+import RelDB._
+
 case class RelDB[K[_]] private (
   private val tables: Map[Rel[_], RelTable[_]],
-  private val patternTriggers: Map[Pattern[_], List[_ => K[Unit]]],
-  private val relToPatterns: Map[Rel[_], Set[OrientedPattern[_, _]]]
-){
+  private val patternTriggers: Map[PartiallyAssignedPattern[_], List[_ => K[Unit]]],
+  private val relToPatterns: Map[Rel[_], Set[PartiallyAssignedOrientedPattern[_, _]]]
+) {
 
   case class Inserter[L <: HList, OS <: HList] private[RelDB] (rel: Rel[L])(implicit m: Mapped.Aux[L, Order, OS]) {
 
@@ -32,7 +34,7 @@ case class RelDB[K[_]] private (
         case Some(tbl1) => // successfully inserted
 
           // search for pattern matches before including the new relation
-          val ks: List[K[Unit]] = watchedPatterns(rel) flatMap { collectTriggers(_, row) }
+          val ks: List[K[Unit]] = collectTriggers(rel, row)
 
           (replaceTable(rel)(tbl1), Foldable[List].sequence_(ks))
       }
@@ -41,22 +43,25 @@ case class RelDB[K[_]] private (
 
   def into[L <: HList](rel: Rel[L])(implicit m: Mapped[L, Order]): Inserter[L, m.Out] = Inserter(rel)(m)
 
-  def addOnPatternMatch[V <: HList](p: Pattern[V])(h: V => K[Unit])(implicit K: Applicative[K]): (RelDB[K], K[Unit]) = {
+  def addOnPatternMatch[V <: HList](p: Pattern[V], ass: Assignment[V])(h: V => K[Unit])(implicit K: Applicative[K]): (RelDB[K], K[Unit]) = {
     require(p.isCovered, "The domain of the pattern is not fully covered by its relations. These positions are not covered: " + ((0 until p.vertexCount).toSet -- p.vertexSet))
 
-    val matches: List[V] = search(p)
+    val matches: List[V] = search(p, ass)
 
     val k = Foldable[List].traverse_(matches)(h(_))
 
-    (addTrigger(p, h), k)
+    (addTrigger(PartiallyAssignedPattern(p, ass), h), k)
   }
 
-  private def collectTriggers[V <: HList, L <: HList](op: OrientedPattern[V, L], l: L): List[K[Unit]] = for {
-    hit <- search(op, l)
-    trig <- triggers(op.pattern)
+  private def collectTriggers[L <: HList](rel: Rel[L], row: L): List[K[Unit]] =
+    watchedPatterns(rel) flatMap { collectTriggers(_, row) }
+
+  private def collectTriggers[V <: HList, L <: HList](paop: PartiallyAssignedOrientedPattern[V, L], row: L): List[K[Unit]] = for {
+    hit <- search(paop, row)
+    trig <- triggers(paop.unorient)
   } yield trig(hit)
 
-  private def search[V <: HList](p: Pattern[V]): List[V] = {
+  private def search[V <: HList](p: Pattern[V], initAss: Assignment[V]): List[V] = {
     // find the size of the table for each relation in the pattern
     val relSizes = p.relations.map(rc => (rc.rel, table0(rc.rel).map(_.size).getOrElse(0)))
 
@@ -64,17 +69,30 @@ case class RelDB[K[_]] private (
     val minRel = relSizes.reduce((r1, r2) => if(r1._2 <= r2._2) r1 else r2)
 
     // start the search from the minimum relation
-    search(p, minRel._1)
+    search(p, initAss, minRel._1)
   }
 
-  private def search[V <: HList, L <: HList](p: Pattern[V], rel: Rel[L]): List[V] = {
-    val op = p.orient(rel)
-    table0(rel).map(_.rows).getOrElse(Vector[L]()).toList.flatMap(search(op, _))
+  private def search[V <: HList, L <: HList](p: Pattern[V], initAss: Assignment[V], rel: Rel[L]): List[V] = {
+    table0(rel) match {
+      case None => Nil
+      case Some(tbl) =>
+        p.orient(rel).orientations flatMap { case (r, rs) =>
+          tbl.query(initAss.get(r.choose)) flatMap { l =>
+            search(initAss.set(r.choose)(l), rs)
+          }
+        }
+    }
   }
 
-  private def search[V <: HList, L <: HList](op: OrientedPattern[V, L], l: L): List[V] =
+  private def search[V <: HList, L <: HList](paop: PartiallyAssignedOrientedPattern[V, L], l: L): List[V] =
+    search(paop.pattern, paop.assignment, l)
+
+  private def search[V <: HList, L <: HList](op: OrientedPattern[V, L], initAss: Assignment[V], l: L): List[V] =
     op.orientations flatMap { case (r, rs) =>
-      search(op.pattern.emptyAssignment.set(r.choose)(l), rs)
+      initAss.extend(r.choose)(l) match {
+        case None => Nil
+        case Some(ass) => search(ass, rs)
+      }
     }
 
   private def search[V <: HList](ass: Assignment[V], rels: List[RelChoice[V, _ <: HList]]): List[V] = rels match {
@@ -94,26 +112,30 @@ case class RelDB[K[_]] private (
     tables.get(rel).map(_.asInstanceOf[RelTable[L]])
 
   // lookup all watched patterns that contain the given rel
-  private def watchedPatterns[L <: HList](rel: Rel[L]): List[OrientedPattern[_ <: HList, L]] =
-    relToPatterns.get(rel).map(_.toList.asInstanceOf[List[OrientedPattern[_ <: HList, L]]]).getOrElse(Nil)
+  private def watchedPatterns[L <: HList](rel: Rel[L]): List[PartiallyAssignedOrientedPattern[_ <: HList, L]] =
+    relToPatterns.get(rel).map(_.toList.asInstanceOf[List[PartiallyAssignedOrientedPattern[_ <: HList, L]]]).getOrElse(Nil)
 
   private def query[L <: HList](rel: Rel[L])(q: Assignment[L]): List[L] = table0(rel) match {
     case Some(tbl) => tbl.query(q)
     case None => Nil
   }
 
-  private def triggers[V <: HList](p: Pattern[V]): List[V => K[Unit]] =
+  private def triggers[V <: HList](p: PartiallyAssignedPattern[V]): List[V => K[Unit]] =
     patternTriggers.getOrElse(p, Nil).asInstanceOf[List[V => K[Unit]]]
 
   private def replaceTable[L <: HList](rel: Rel[L])(tbl: RelTable[L]): RelDB[K] =
     copy(tables = tables + ((rel, tbl)))
 
-  private def addTrigger[V <: HList](p: Pattern[V], h: V => K[Unit]): RelDB[K] =
+  private def addTrigger[V <: HList](p: PartiallyAssignedPattern[V], h: V => K[Unit]): RelDB[K] =
     copy(patternTriggers = patternTriggers + ((p, h :: triggers(p))))
-
 }
 
 object RelDB {
+
+  private[rel] case class PartiallyAssignedPattern[V <: HList](pattern: Pattern[V], assignment: Assignment[V])
+  private[rel] case class PartiallyAssignedOrientedPattern[V <: HList, L <: HList](pattern: OrientedPattern[V, L], assignment: Assignment[V]) {
+    def unorient: PartiallyAssignedPattern[V] = PartiallyAssignedPattern(pattern.pattern, assignment)
+  }
 
   def empty[K[_]]: RelDB[K] = RelDB(
     Map.empty,
@@ -125,7 +147,7 @@ object RelDB {
 
     def step0[K[_] : Applicative, A](f: RelLang[K, A])(db: RelDB[K]): (RelDB[K], K[A]) = f match {
       case r @ Relate(rel, values) => db.into(rel)(r.ordersWitness).insert(values)(r.orders, Applicative[K])
-      case OnPatternMatch(p, h) => db.addOnPatternMatch(p)(h)
+      case OnPatternMatch(p, a, h) => db.addOnPatternMatch(p, a)(h)
     }
 
   }
--- src/main/scala/nutcracker/rel/RelLang.scala
+++ src/main/scala/nutcracker/rel/RelLang.scala
@@ -1,6 +1,7 @@
 package nutcracker.rel
 
 import algebra.Order
+import nutcracker.rel.RelDB.PartiallyAssignedPattern
 import nutcracker.util.Mapped
 import nutcracker.util.free.{InjectK, FreeK}
 
@@ -16,10 +17,10 @@ object RelLang {
     val orders: OS = os
     val ordersWitness: Mapped.Aux[L, Order, OS] = m
   }
-  case class OnPatternMatch[K[_], V <: HList](p: Pattern[V], h: V => K[Unit]) extends RelLang[K, Unit]
+  case class OnPatternMatch[K[_], V <: HList](p: Pattern[V], a: Assignment[V], h: V => K[Unit]) extends RelLang[K, Unit]
 
   def relate[K[_], L <: HList, OS <: HList](rel: Rel[L], values: L)(implicit m: Mapped.Aux[L, Order, OS], os: OS): RelLang[K, Unit] = Relate(rel, values)
-  def onPatternMatch[K[_], V <: HList](p: Pattern[V], h: V => K[Unit]): RelLang[K, Unit] = OnPatternMatch(p, h)
+  def onPatternMatch[K[_], V <: HList](p: Pattern[V], a: Assignment[V], h: V => K[Unit]): RelLang[K, Unit] = OnPatternMatch(p, a, h)
 
 
   private type FR[A] = FreeK[RelLang, A]
@@ -30,6 +31,6 @@ object RelLang {
   }
 
   def onPatternMatchF[F[_[_], _], V <: HList](p: Pattern[V])(h: V => FreeK[F, Unit])(implicit inj: InjectK[RelLang, F]): FreeK[F, Unit] =
-    FreeK.lift(onPatternMatch[FreeK[F, ?], V](p, h))
+    FreeK.lift(onPatternMatch[FreeK[F, ?], V](p, p.emptyAssignment, h))
 
 }
