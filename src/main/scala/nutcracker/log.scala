053b663044f779d15b02c264544663e7e68ab46b
Tomas Mikula <tomas.mikula@gmail.com>
2016-04-29 00:21:52 -0400

    Add object nutcracker.log with convenient methods to use a List as a log, as a cell in PropagationStore.

---
+++ src/main/scala/nutcracker/log.scala
@@ -0,0 +1,35 @@
+package nutcracker
+
+import scala.language.implicitConversions
+
+import nutcracker.Dom.Status
+import nutcracker.PropagationLang._
+import nutcracker.util.FreeK
+
+/** Convenience methods to work with a list as a prepend-only log.
+  * Monotonic update is prepending an element to the list.
+  * A diff is a list of appended elements.
+  */
+object log {
+
+  type LogRef[A] = DRef[List[A], A, List[A]]
+
+  final case class LogOps[A](l: LogRef[A]) extends AnyVal {
+    def write(a: A): FreeK[PropagationLang, Unit] = log(l, a)
+  }
+
+  implicit def logOps[A](l: LogRef[A]): LogOps[A] = LogOps(l)
+
+  def newLog[A]: FreeK[PropagationLang, LogRef[A]] = cellF(List[A]())
+  def log[A](ref: LogRef[A], a: A): FreeK[PropagationLang, Unit] = updateF(ref)(a)
+
+  implicit def logDom[A]: Dom[List[A], A, List[A]] = new Dom[List[A], A, List[A]] {
+
+    def update(d: List[A], u: A): Option[(List[A], List[A])] =
+      Some((u :: d, List(u)))
+
+    def combineDiffs(d1: List[A], d2: List[A]): List[A] = d2 ::: d1
+
+    def assess(d: List[A]): Status[A] = Dom.Refined
+  }
+}
