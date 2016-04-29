90885d09f6563eee708c5ae7451a76b791c72568
Tomas Mikula <tomas.mikula@gmail.com>
2016-04-29 03:36:47 -0400

    Add variance to DRef type parameters.

--- src/main/scala/nutcracker/DRef.scala
+++ src/main/scala/nutcracker/DRef.scala
@@ -10,22 +10,27 @@ import nutcracker.util.{FreeK, Inject, InjectK}
 
 import scalaz.Cont
 
-sealed trait VRef[D]
+sealed trait VRef[+D]
 
-sealed case class DRef[D, U, Δ](domainId: Long) extends VRef[D] {
-
-  def ==>[U1](target: DRef[D, U1, _])(implicit inj: Inject[Meet[D], U1]): FreeK[PropagationLang, Unit] =
-    valTriggerF(this){ d => fireReload(intersectF(target)(d)) }
-  def <=>(target: DRef[D, U, _])(implicit inj: Inject[Meet[D], U]): FreeK[PropagationLang, Unit] =
-    (this ==> target) >> (target ==> this)
-  def >>=[F[_[_], _], A](f: A => FreeK[F, Unit])(implicit inj: InjectK[PropagationLang, F], ee: EmbedExtract[A, D]): FreeK[F, Unit] =
-    whenResolvedF(this)(f)
-
-  def asCont[F[_[_], _], A](implicit inj: InjectK[PropagationLang, F], ee: EmbedExtract[A, D]): Cont[FreeK[F, Unit], A] =
-    Cont { whenResolvedF(DRef.this)(_) }
-}
+sealed case class DRef[+D, -U, +Δ](domainId: Long) extends VRef[D]
 
 object DRef {
+
+  implicit def drefOps[D, U, Δ](ref: DRef[D, U, Δ]) = DRefOps(ref)
+
+  final case class DRefOps[D, U, Δ](ref: DRef[D, U, Δ]) extends AnyVal {
+    def ==>[U1](target: DRef[D, U1, _])(implicit inj: Inject[Meet[D], U1]): FreeK[PropagationLang, Unit] =
+      valTriggerF(ref){ d => fireReload(intersectF(target)(d)) }
+    def <=>(target: DRef[D, U, _])(implicit inj: Inject[Meet[D], U]): FreeK[PropagationLang, Unit] =
+      (ref ==> target) >> (target ==> ref)
+    def >>=[F[_[_], _], A](f: A => FreeK[F, Unit])(implicit inj: InjectK[PropagationLang, F], ee: EmbedExtract[A, D]): FreeK[F, Unit] =
+      whenResolvedF(ref)(f)
+
+    def asCont[F[_[_], _], A](implicit inj: InjectK[PropagationLang, F], ee: EmbedExtract[A, D]): Cont[FreeK[F, Unit], A] =
+      Cont { whenResolvedF(ref)(_) }
+
+  }
+
   implicit def toCont[F[_[_], _], A, D](ref: DRef[D, _, _])(implicit inj: InjectK[PropagationLang, F], ee: EmbedExtract[A, D]): Cont[FreeK[F, Unit], A] =
     ref.asCont
 }
