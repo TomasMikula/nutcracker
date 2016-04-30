f2a0482a9ee25e6ea88aab7187ae4e7e225438b9
Tomas Mikula <tomas.mikula@gmail.com>
2016-04-30 17:49:46 -0400

    Add KMapB, a KMap with an upper bound on type param to K[_] and V[_].

--- src/main/scala/nutcracker/util/KMap.scala
+++ src/main/scala/nutcracker/util/KMap.scala
@@ -24,6 +24,28 @@ object KMap {
   def apply[K[_], V[_]](): KMap[K, V] = KMap[K, V](Map[K[_], V[_]]())
 }
 
+/** KMap with an upper bound on the type parameter accepted by K[_], V[_]. */
+final case class KMapB[K[_ <: UB], V[_ <: UB], UB](map: Map[K[_], V[_]]) extends AnyVal {
+  def isEmpty: Boolean = map.isEmpty
+  def nonEmpty: Boolean = map.nonEmpty
+  def head: (K[A], V[A]) forSome { type A <: UB } = map.head.asInstanceOf[(K[A], V[A]) forSome { type A <: UB }]
+  def tail: KMapB[K, V, UB] = KMapB[K, V, UB](map.tail)
+  def apply[A <: UB](k: K[A]): V[A] = map(k).asInstanceOf[V[A]]
+  def get[A <: UB](k: K[A]): Option[V[A]] = map.get(k).asInstanceOf[Option[V[A]]]
+  def getOrElse[A <: UB](k: K[A], default: => V[A]): V[A] = get(k).getOrElse(default)
+  def updated[A <: UB](k: K[A], v: V[A]): KMapB[K, V, UB] = KMapB[K, V, UB](map.updated(k, v))
+  def updated[A <: UB](k: K[A], v: V[A], combineIfPresent: (V[A], V[A]) => V[A]): KMapB[K, V, UB] =
+    get(k) match {
+      case None => updated(k, v)
+      case Some(v0) => updated(k, combineIfPresent(v0, v))
+    }
+  def -(k: K[_]): KMapB[K, V, UB] = KMapB[K, V, UB](map - k)
+}
+
+object KMapB {
+  def apply[K[_], V[_], UB](): KMapB[K, V, UB] = KMapB[K, V, UB](Map[K[_], V[_]]())
+}
+
 
 final case class K2Map[K[_, _], V[_, _]](map: Map[K[_, _], V[_, _]]) extends AnyVal {
   def isEmpty: Boolean = map.isEmpty
