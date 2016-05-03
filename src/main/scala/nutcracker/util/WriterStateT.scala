4ea0ea6242844bb87fd581f034778d2a243d229a
Tomas Mikula <tomas.mikula@gmail.com>
2016-05-03 01:49:16 -0400

    Add BindRec instance for WriterStateT.

--- src/main/scala/nutcracker/util/WriterStateT.scala
+++ src/main/scala/nutcracker/util/WriterStateT.scala
@@ -1,7 +1,7 @@
 package nutcracker.util
 
 import scala.language.higherKinds
-import scalaz.{Functor, Lens, Monad, MonadTrans, Monoid}
+import scalaz.{-\/, BindRec, Functor, Lens, Monad, MonadTrans, Monoid, \/, \/-}
 
 final case class WriterStateT[F[_], W, S, A](run: S => F[(W, S, A)]) extends AnyVal {
   def apply(s: S): F[(W, S, A)] = run(s)
@@ -23,10 +23,32 @@ final case class WriterStateT[F[_], W, S, A](run: S => F[(W, S, A)]) extends Any
 object WriterStateT {
   implicit def monad[F[_], W, S](implicit F: Monad[F], W: Monoid[W]): Monad[WriterStateT[F, W, S, ?]] =
     new Monad[WriterStateT[F, W, S, ?]] {
-      override def point[A](a: => A): WriterStateT[F, W, S, A] = WriterStateT(s => F.point((W.zero, s, a)))
-      override def map[A, B](fa: WriterStateT[F, W, S, A])(f: A => B): WriterStateT[F, W, S, B] = fa map f
+      override def point[A](a: => A): WriterStateT[F, W, S, A] =
+        WriterStateT(s => F.point((W.zero, s, a)))
+      override def map[A, B](fa: WriterStateT[F, W, S, A])(f: A => B): WriterStateT[F, W, S, B] =
+        fa.map(f)
       override def bind[A, B](fa: WriterStateT[F, W, S, A])(f: A => WriterStateT[F, W, S, B]): WriterStateT[F, W, S, B] =
-        fa flatMap f
+        fa.flatMap(f)
+    }
+
+  implicit def bindRec[F[_], W, S](implicit F0: BindRec[F], F1: Monad[F], W: Monoid[W]): BindRec[WriterStateT[F, W, S, ?]] =
+    new BindRec[WriterStateT[F, W, S, ?]] {
+      def tailrecM[A, B](f: A => WriterStateT[F, W, S, A \/ B])(a: A): WriterStateT[F, W, S, B] = {
+        def go(x: (W, S, A)): F[(W, S, A) \/ (W, S, B)] = {
+          val (w, s, a) = x
+          F0.map(f(a)(s)) {
+            case (w1, s1, \/-(b)) => \/-((W.append(w, w1), s1, b))
+            case (w1, s1, -\/(a1)) => -\/((W.append(w, w1), s1, a1))
+          }
+        }
+        WriterStateT(s => F0.tailrecM(go)((W.zero, s, a)))
+      }
+
+      def bind[A, B](fa: WriterStateT[F, W, S, A])(f: A => WriterStateT[F, W, S, B]): WriterStateT[F, W, S, B] =
+        fa.flatMap(f)
+
+      def map[A, B](fa: WriterStateT[F, W, S, A])(f: A => B): WriterStateT[F, W, S, B] =
+        fa.map(f)(F0)
     }
 
   implicit def monadTrans[W, S](implicit W: Monoid[W]): MonadTrans[WriterStateT[?[_], W, S, ?]] =
@@ -38,3 +60,4 @@ object WriterStateT {
         monad[G, W, S]
     }
 }
+
