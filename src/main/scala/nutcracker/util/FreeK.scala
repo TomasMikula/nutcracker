d803632b9b79ee5639e541823e331318272d9ed6
Tomas Mikula <tomas.mikula@gmail.com>
2016-05-12 00:07:05 -0400

    Polish.

--- src/main/scala/nutcracker/util/FreeK.scala
+++ src/main/scala/nutcracker/util/FreeK.scala
@@ -7,30 +7,42 @@ import scalaz.Id._
 final case class FreeK[F[_[_], _], A](run: FreeT[F[FreeK[F, ?], ?], Id, A]) extends AnyVal {
   import FreeK._
 
-  def flatMap[B](f: A => FreeK[F, B]): FreeK[F, B] = bind(this)(f)
-  def >>=[B](f: A => FreeK[F, B]): FreeK[F, B] = flatMap(f)
-  def >>[B](fb: => FreeK[F, B])(implicit AIsUnit: A =:= Unit): FreeK[F, B] = this >>= { _ => fb }
+  def flatMap[B](f: A => FreeK[F, B]): FreeK[F, B] =
+    FreeK(run.flatMap(f(_).run))
+
+  def >>=[B](f: A => FreeK[F, B]): FreeK[F, B] =
+    flatMap(f)
+
+  def >>[B](fb: => FreeK[F, B])(implicit AIsUnit: A =:= Unit): FreeK[F, B] =
+    this >>= { _ => fb }
+
   def >>>=[G[_[_], _], B](f: A => FreeK[G, B])(implicit
     inj: InjectK[F, G],
     FK: FunctorKA[F]
-  ): FreeK[G, B] = injectTransform(inj, FK)(this) flatMap f
+  ): FreeK[G, B] =
+    injectTransform(inj, FK)(this) flatMap f
+
   def >>>[G[_[_], _], B](gb: => FreeK[G, B])(implicit
     AIsUnit: A =:= Unit,
     inj: InjectK[F, G],
     FK: FunctorKA[F]
-  ): FreeK[G, B] = this >>>= { _ => gb }
-  def map[B](f: A => B): FreeK[F, B] = FreeK(run.map(f))
+  ): FreeK[G, B] =
+    this >>>= { _ => gb }
 
-  def inj[G[_[_], _]](implicit tr: (FreeK[F, ?] ~> FreeK[G, ?])): FreeK[G, A] = tr(this)
+  def map[B](f: A => B): FreeK[F, B] =
+    FreeK(run.map(f))
+
+  def inj[G[_[_], _]](implicit tr: (FreeK[F, ?] ~> FreeK[G, ?])): FreeK[G, A] =
+    tr(this)
 
   // sometimes it is easier for scalac to find these implicit arguments than for inj above
   def inject[G[_[_], _]](implicit
     inj: InjectK[F, G],
-    FK: FunctorKA[F]): FreeK[G, A] = FreeK.injectTransform(inj, FK)(this)
+    FK: FunctorKA[F]
+  ): FreeK[G, A] =
+    FreeK.injectTransform(inj, FK)(this)
 
-  type K[T] = FreeK[F, T]
-
-  final def foldMap[M[_]](tr: F[K, ?] ~> M)(implicit M0: BindRec[M], M1: Applicative[M]): M[A] =
+  final def foldMap[M[_]](tr: F[FreeK[F, ?], ?] ~> M)(implicit M0: BindRec[M], M1: Applicative[M]): M[A] =
     run.hoist(idToM[M]).foldMap(tr)
 }
 
@@ -42,15 +54,12 @@ object FreeK {
   def liftF[F[_[_], _], A](a: F[FreeK[F, ?], A]): FreeK[F, A] =
     FreeK(FreeT.liftF[F[FreeK[F, ?], ?], Id, A](a))
 
-  def bind[F[_[_], _], A1, A2](fa: FreeK[F, A1])(f: A1 => FreeK[F, A2]): FreeK[F, A2] =
-    FreeK(fa.run.flatMap(f(_).run))
-
   def injLiftF[F[_[_], _], G[_[_], _], A](a: F[FreeK[G, ?], A])(implicit inj: InjectK[F, G]): FreeK[G, A] =
     liftF(inj(a))
 
   implicit def freeKMonad[F[_[_], _]]: Monad[FreeK[F, ?]] = new Monad[FreeK[F, ?]] {
     def point[A](a: => A): FreeK[F, A] = FreeK.pure(a)
-    def bind[A, B](fa: FreeK[F, A])(f: A => FreeK[F, B]): FreeK[F, B] = FreeK.bind(fa)(f)
+    def bind[A, B](fa: FreeK[F, A])(f: A => FreeK[F, B]): FreeK[F, B] = fa.flatMap(f)
   }
 
   implicit def injectTransform[F[_[_], _], G[_[_], _]](implicit
