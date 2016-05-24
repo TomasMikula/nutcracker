dc7b42d087612c547e237d0c452a756858bc3271
Tomas Mikula <tomas.mikula@gmail.com>
2016-05-24 15:59:59 -0400

    Add some operations to IncSet.

--- src/main/scala/nutcracker/IncSet.scala
+++ src/main/scala/nutcracker/IncSet.scala
@@ -1,5 +1,11 @@
 package nutcracker
 
+import scala.language.higherKinds
+import nutcracker.PropagationLang._
+import nutcracker.util.{FreeK, InjectK}
+
+import scalaz.{Codensity, Foldable, Monad}
+
 /** Increasing set.
   * A wrapper for `Set` where a _monotonic_ update is one that adds
   * elements, e.g. by union with another set.
@@ -14,6 +20,7 @@ final class IncSet[A] private(private val value: Set[A]) extends AnyVal {
 
 object IncSet {
   def apply[A](as: A*): IncSet[A] = new IncSet(Set(as: _*))
+  def empty[A]: IncSet[A] = new IncSet(Set())
   def singleton[A](a: A): IncSet[A] = new IncSet(Set(a))
   def wrap[A](as: Set[A]): IncSet[A] = new IncSet(as)
 
@@ -39,4 +46,34 @@ object IncSet {
       Diff(d1.value union d2.value)
   }
 
+  def insert[A](add: Set[A], into: IncSetRef[A]): FreeK[PropagationLang, Unit] =
+    PropagationLang.updateF(into)(Join(wrap(add)))
+
+  def include[A](sub: IncSetRef[A], sup: IncSetRef[A]): FreeK[PropagationLang, Unit] =
+    domTriggerF(sub)((sa: IncSet[A]) => {
+      val now = Some(insert(sa.value, sup))
+      val onChange = Some((sa: IncSet[A], delta: Diff[Set[A]]) => FireReload(insert(delta.value, sup)))
+      (now, onChange)
+    })
+
+  /** Relative monadic bind. [[nutcracker.IncSet.IncSetRef]] is a monad relative to `FreeK[F, ?]`,
+    * i.e. we can implement `bind` if additional effects of type `FreeK[F, ?]` are allowed.
+    * This is equivalent to having a monad instance for `λ[A => FreeK[F, IncSetRef[A]]]`.
+    */
+  def relBind[F[_[_], _], A, B](sref: IncSetRef[A])(f: A => FreeK[F, IncSetRef[B]])(implicit inj: InjectK[PropagationLang, F]): FreeK[F, IncSetRef[B]] = for {
+    res <- cellF(IncSet.empty[B]).inject[F]
+    _ <- domTriggerF[F, IncSet[A]](sref)((sa: IncSet[A]) => {
+      val now = concat(sa.toList.map(f(_) >>= (refb => include(refb, res))))
+      val onChange = Some((sa: IncSet[A], delta: Diff[Set[A]]) => FireReload(concat(delta.value.toList.map(f(_) >>= (refb => include(refb, res))))))
+      (Some(now), onChange)
+    })
+  } yield res
+
+  implicit def monad[F[_[_], _]](implicit inj: InjectK[PropagationLang, F]): Monad[λ[A => FreeK[F, IncSetRef[A]]]] =
+    new Monad[λ[A => FreeK[F, IncSetRef[A]]]] {
+      def point[A](a: => A): FreeK[F, IncSetRef[A]] = cellF(singleton(a)).inject[F]
+
+      def bind[A, B](fa: FreeK[F, IncSetRef[A]])(f: A => FreeK[F, IncSetRef[B]]): FreeK[F, IncSetRef[B]] =
+        fa.flatMap(sa => relBind(sa)(f))
+    }
 }
