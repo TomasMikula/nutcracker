61ccb476e8241bc5246bd54f79b788c14c2fd205
Tomas Mikula <tomas.mikula@gmail.com>
2016-10-12 14:42:35 -0400

    Factor out dom syntax into a trait (DomSyntax). Add isFailed to syntax.

--- src/main/scala/nutcracker/syntax/dom.scala
+++ src/main/scala/nutcracker/syntax/dom.scala
@@ -2,10 +2,13 @@ package nutcracker.syntax
 
 import nutcracker.Dom
 
-object dom {
+object dom extends DomSyntax
+
+trait DomSyntax {
   implicit class DomOps[D, U, Δ](d: D)(implicit dom: Dom.Aux[D, U, Δ]) {
     def update(u: U): Option[(D, Δ)] = dom.update(d, u)
     def update_(u: U): D = dom.update_(d, u)
     def assess: Dom.Status[U] = dom.assess(d)
+    def isFailed: Boolean = dom.isFailed(d)
   }
 }
