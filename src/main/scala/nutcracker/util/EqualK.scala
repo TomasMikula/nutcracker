1aff070210f5a16971531f5b4b9edf4a9ff0beea
Tomas Mikula <tomas.mikula@gmail.com>
2016-11-21 18:36:49 -0500

    HEqualK not to extend EqualK. Provide implicit conversion instead.

--- src/main/scala/nutcracker/util/EqualK.scala
+++ src/main/scala/nutcracker/util/EqualK.scala
@@ -13,12 +13,16 @@ trait EqualK[F[_]] {
 
 object EqualK {
   implicit def specialize[F[_], A](implicit ev: EqualK[F]): Equal[F[A]] = ev.specialize[A]
+
+  implicit def homogenize[F[_]](implicit ev: HEqualK[F]): EqualK[F] = ev.homogenize
 }
 
 /** Equality for type constructors of kind `* -> *` applied to possibly heterogenous types. */
-trait HEqualK[F[_]] extends EqualK[F] {
+trait HEqualK[F[_]] {
   def hEqual[A, B](fa: F[A], fb: F[B]): Boolean
 
-  override def equal[A](f1: F[A], f2: F[A]): Boolean =
-    hEqual(f1, f2)
+  def homogenize: EqualK[F] = new EqualK[F] {
+    override def equal[A](f1: F[A], f2: F[A]): Boolean =
+      hEqual(f1, f2)
+  }
 }
