100c422fbb6332dade97b754b1291218995e0e1a
Tomas Mikula <tomas.mikula@gmail.com>
2016-12-08 20:12:55 -0500

    Add ShowK.fromToString.

--- src/main/scala/nutcracker/util/ShowK.scala
+++ src/main/scala/nutcracker/util/ShowK.scala
@@ -3,3 +3,9 @@ package nutcracker.util
 trait ShowK[F[_]] {
   def shows[A](fa: F[A]): String
 }
+
+object ShowK {
+  def fromToString[F[_]]: ShowK[F] = new ShowK[F] {
+    def shows[A](fa: F[A]): String = fa.toString
+  }
+}
