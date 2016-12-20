aed83e7e07d39348ecf4a9945a4421d4c9164c1f
Tomas Mikula <tomas.mikula@gmail.com>
2016-12-19 19:58:50 -0500

    DeepShow instance for Antichain.

--- src/main/scala/nutcracker/Antichain.scala
+++ src/main/scala/nutcracker/Antichain.scala
@@ -2,7 +2,7 @@ package nutcracker
 
 import nutcracker.Dom.{Refined, Status}
 import nutcracker.ops._
-import nutcracker.util.{ContU, DeepEqual, IsEqual, Uninhabited}
+import nutcracker.util.{ContU, DeepEqual, DeepShow, Desc, IsEqual, Uninhabited}
 
 import scala.language.higherKinds
 import scalaz.{Bind, ContT, Monad, Show}
@@ -74,6 +74,11 @@ trait AntichainInstances extends AntichainInstances1 {
   implicit def deepEqualInstance1[Ptr1[_], Ptr2[_], A1, A2](implicit ev: DeepEqual[A1, A2, Ptr1, Ptr2]): DeepEqual[Antichain[A1], A2, Ptr1, Ptr2] =
     new DeepEqual[Antichain[A1], A2, Ptr1, Ptr2] {
       def equal(a1: Antichain[A1], a2: A2): IsEqual[Ptr1, Ptr2] = ev.equal(a1.value, a2)
+    }
+
+  implicit def deepShowInstance[Ptr[_], A](implicit ev: DeepShow[A, Ptr]): DeepShow[Antichain[A], Ptr] =
+    new DeepShow[Antichain[A], Ptr] {
+      def show(a: Antichain[A]): Desc[Ptr] = ev.show(a.value)
     }
 
 }
