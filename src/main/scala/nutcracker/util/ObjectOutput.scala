46811dbdc8f4a88bf0d80a5b36fda79bceac9d22
Tomas Mikula <tomas.mikula@gmail.com>
2017-01-17 16:21:26 -0500

    Rename FreeOutput to FreeObjectOutput. Represent via Free.

--- src/main/scala/nutcracker/util/ObjectOutput.scala
+++ src/main/scala/nutcracker/util/ObjectOutput.scala
@@ -1,8 +1,9 @@
 package nutcracker.util
 
-import scala.annotation.tailrec
+import nutcracker.util.free.Free
+
 import scalaz.Id._
-import scalaz.{-\/, Leibniz, Monoid, \/, \/-, ~>}
+import scalaz.{-\/, Monoid, \/, \/-, ~>}
 
 /**
   *
@@ -15,22 +16,8 @@ trait ObjectOutput[O, R, Ptr[_]] {
   def writeObject[A](out: O, pa: Ptr[A])(implicit ser: ObjectSerializer[A, R, Ptr]): O
 }
 
-import FreeOutput.Mode
-
-// isomorphic to List[R \/ (Ptr[A], ObjectSerializer[A, R, Ptr]) forSome { type A }]
-sealed abstract class FreeOutput[R, Ptr[_], Mod <: Mode] {
-  import FreeOutput._
-
-  def reverse: FreeOutput[R, Ptr, Mod#Reverse] = {
-    @tailrec def go(arg: FreeOutput[R, Ptr, Mod], acc: FreeOutput[R, Ptr, Mod#Reverse]): FreeOutput[R, Ptr, Mod#Reverse] =
-      arg match {
-        case Write(rest, s) => go(rest, Write(acc, s))
-        case WriteObject(rest, pa, ser) => go(rest, WriteObject(acc, pa, ser))
-        case Empty() => acc
-      }
-
-    go(this, empty)
-  }
+final class FreeObjectOutput[R, Ptr[_]] private[FreeObjectOutput] (private val unwrap: Free[FreeObjectOutput.OutputInst[R, Ptr, ?], Unit]) /* extends AnyVal // can't have nested AnyVals :( */ {
+  import FreeObjectOutput._
 
   /**
     *
@@ -47,60 +34,41 @@ sealed abstract class FreeOutput[R, Ptr[_], Mod <: Mode] {
     * @tparam S1 state to be passed downwards
     * @tparam S2 state to be passed upwards
     */
-  def foldBiState[S1, S2](s1: S1)(step: S1 => Ptr ~> λ[α => (S1, S2 => (S2, Decoration[R])) \/ (S2, R)])(deref: Ptr ~> Id)(implicit S2: Monoid[S2], ev: Leibniz[Nothing, Mode, Mod, Append]): Lst[R] = {
-    case class StackFrame(s1: S1, s2acc: S2, pred: Lst[R], tr: S2 => (S2, Decoration[R]), tail: FreeOutput[R, Ptr, Prepend])
-    @tailrec def go(s1: S1, s2acc: S2, pred: Lst[R], tail: FreeOutput[R, Ptr, Prepend], stack: List[StackFrame]): Lst[R] =
-      tail match {
-        case Write(tail, r) => go(s1, s2acc, pred :+ r, tail, stack)
-        case WriteObject(tail, pa, ser) => step(s1)(pa) match {
-          case -\/((s11, tr)) => go(s11, S2.zero, Lst.empty, ser.write(FreeOutput.empty[R, Ptr, Append], deref(pa)).reverse, StackFrame(s1, s2acc, pred, tr, tail) :: stack)
-          case \/-((s2, r)) => go(s1, S2.append(s2acc, s2), pred :+ r, tail, stack)
-        }
-        case Empty() => stack match {
-          case StackFrame(s10, s20, ss, tr, tail) :: stack =>
-            val (s2, dec) = tr(s2acc)
-            val acc = dec match {
-              case Naked => pred
-              case BeforeAfter(before, after) => before +: pred :+ after
-              case Before(r) => r +: pred
-              case After(r) => pred :+ r
-            }
-            go(s10, S2.append(s20, s2), ss ++ acc, tail, stack)
-          case Nil => pred
+  private def foldBiState[S1, S2](s1: S1)(step: S1 => Ptr ~> λ[α => (S1, S2 => (S2, Decoration[R])) \/ (S2, R)])(deref: Ptr ~> Id)(implicit S2: Monoid[S2]): Lst[R] = {
+    type S2_ = (S2, Lst[R]); import scalaz.std.tuple.tuple2Monoid
+    unwrap.foldRunRecParM[Id, S1, S2_](s1, λ[λ[α => (S1, OutputInst[R, Ptr, α])] ~> λ[α => (S1, Free[OutputInst[R, Ptr, ?], α], S2_ => S2_) \/ (S2_, α)]] {
+      case (s1, inst) => inst match {
+        case Write(r) => \/-(((S2.zero, Lst.singleton(r)), ()))
+        case WriteObject(pa, ser) => step(s1)(pa) match {
+          case -\/((s11, tr)) => -\/((s11, ser.write(FreeObjectOutput.empty[R, Ptr], deref(pa)).unwrap, { case (s2, r) => tr(s2) match { case (s2, decor) => (s2, decor.decorate(r)(_ +: _, _ :+ _)) }}))
+          case \/-((s2, r)) => \/-(((s2, Lst.singleton(r)), ()))
         }
       }
-
-    go(s1, S2.zero, Lst.empty, ev.subst[({ type Out[m <: Mode] = FreeOutput[R, Ptr, m]})#Out](this).reverse, Nil)
+    })._1._2
   }
 
   /** Specialized [[foldBiState]] for `S2 = Unit`. */
-  def foldState[S](s: S)(step: S => Ptr ~> λ[α => (S, Decoration[R]) \/ R])(deref: Ptr ~> Id)(implicit ev: Leibniz[Nothing, Mode, Mod, Append]): Lst[R] = {
-    case class StackFrame(s: S, tail: FreeOutput[R, Ptr, Prepend])
-    @tailrec def go(s: S, out: Lst[R], tail: FreeOutput[R, Ptr, Prepend], stack: List[StackFrame]): Lst[R] =
-      tail match {
-        case Write(tail, r) => go(s, out :+ r, tail, stack)
-        case WriteObject(tail, pa, ser) => step(s)(pa) match {
-          case -\/((s1, dec)) => go(s1, out :+? dec.beforeOption, ser.write(FreeOutput.empty[R, Ptr, Append], deref(pa)).reverse, StackFrame(s, dec.afterOption.map(Write(tail, _)).getOrElse(tail)) :: stack)
-          case \/-(r) => go(s, out :+ r, tail, stack)
-        }
-        case Empty() => stack match {
-          case StackFrame(s0, tail) :: stack => go(s0, out, tail, stack)
-          case Nil => out
+  private def foldState[S](s: S)(step: S => Ptr ~> λ[α => (S, Decoration[R]) \/ R])(deref: Ptr ~> Id): Lst[R] = {
+    type S2 = Lst[R]
+    unwrap.foldRunRecParM[Id, S, S2](s, λ[λ[α => (S, OutputInst[R, Ptr, α])] ~> λ[α => (S, Free[OutputInst[R, Ptr, ?], α], S2 => S2) \/ (S2, α)]] {
+      case (s, inst) => inst match {
+        case Write(r) => \/-((Lst.singleton(r), ()))
+        case WriteObject(pa, ser) => step(s)(pa) match {
+          case -\/((s1, decor)) => -\/((s1, ser.write(FreeObjectOutput.empty[R, Ptr], deref(pa)).unwrap, r => decor.decorate(r)(_ +: _, _ :+ _)))
+          case \/-(r) => \/-((Lst.singleton(r), ()))
         }
       }
-
-    go(s, Lst.empty, ev.subst[({ type Out[m <: Mode] = FreeOutput[R, Ptr, m]})#Out](this).reverse, Nil)
+    })._1
   }
 
-  /** Serialize, cutting off cycles. Pointers that would cause cycles will be handled by `showPtr`. */
+  /** Serialize, cutting off cycles. Pointers that would cause cycles will be handled by `showReference`. */
   def show(
     deref: Ptr ~> Id,
     decorateReferenced: Ptr ~> λ[α => Decoration[R]],
     decorateUnreferenced: Ptr ~> λ[α => Decoration[R]],
     showReference: Ptr ~> λ[α => R]
   )(implicit
-    E: HEqualK[Ptr],
-    ev: Leibniz[Nothing, Mode, Mod, Append]
+    E: HEqualK[Ptr]
   ): Lst[R] = {
     type S1 = List[Ptr[_]] // context, i.e. all parents
     type S2 = Lst[Ptr[_]]  // referenced within the subgraph
@@ -118,14 +86,13 @@ sealed abstract class FreeOutput[R, Ptr[_], Mod <: Mode] {
     foldBiState[S1, S2](Nil)(step)(deref)
   }
 
-  /** Serialize, cutting off cycles. Pointers that would cause cycles will be handled by `showPtr`. */
+  /** Serialize, cutting off cycles. Pointers that would cause cycles will be handled by `showReference`. */
   def show(
     deref: Ptr ~> Id,
     decorateContent: Ptr ~> λ[α => Decoration[R]],
     showReference: Ptr ~> λ[α => R]
   )(implicit
-    E: HEqualK[Ptr],
-    ev: Leibniz[Nothing, Mode, Mod, Append]
+    E: HEqualK[Ptr]
   ): Lst[R] = {
     type S = List[Ptr[_]] // context, i.e. all parents
 
@@ -139,30 +106,38 @@ sealed abstract class FreeOutput[R, Ptr[_], Mod <: Mode] {
   }
 }
 
-object FreeOutput {
-  sealed trait Mode { type Reverse <: Mode }
-  sealed trait Append extends Mode { type Reverse = Prepend }
-  sealed trait Prepend extends Mode { type Reverse = Append }
+object FreeObjectOutput {
+  private[util] sealed trait OutputInst[R, Ptr[_], A]
+  private[FreeObjectOutput] case class Write[R, Ptr[_]](r: R) extends OutputInst[R, Ptr, Unit]
+  private[FreeObjectOutput] case class WriteObject[R, Ptr[_], A](pa: Ptr[A], ser: ObjectSerializer[A, R, Ptr]) extends OutputInst[R, Ptr, Unit]
 
-  private case class Empty[R, Ptr[_], Mod <: Mode]() extends FreeOutput[R, Ptr, Mod]
-  private case class Write[R, Ptr[_], Mod <: Mode](rest: FreeOutput[R, Ptr, Mod], r: R) extends FreeOutput[R, Ptr, Mod]
-  private case class WriteObject[R, Ptr[_], Mod <: Mode, A](rest: FreeOutput[R, Ptr, Mod], pa: Ptr[A], ser: ObjectSerializer[A, R, Ptr]) extends FreeOutput[R, Ptr, Mod]
+  def empty[R, Ptr[_]]: FreeObjectOutput[R, Ptr] = new FreeObjectOutput(Free.point[OutputInst[R, Ptr, ?], Unit](()))
+  def write[R, Ptr[_]](r: R): FreeObjectOutput[R, Ptr] = new FreeObjectOutput(Free.liftF[OutputInst[R, Ptr, ?], Unit](Write(r)))
+  def writeObject[R, Ptr[_], A](pa: Ptr[A], ser: ObjectSerializer[A, R, Ptr]): FreeObjectOutput[R, Ptr] = new FreeObjectOutput(Free.liftF[OutputInst[R, Ptr, ?], Unit](WriteObject(pa, ser)))
 
   sealed abstract class Decoration[+R] {
     def beforeOption: Option[R]
     def afterOption: Option[R]
+    def decorate[S](s: S)(prepend: (R, S) => S, append: (S, R) => S): S = this match {
+      case Naked => s
+      case BeforeAfter(r1, r2) => append(prepend(r1, s), r2)
+      case Before(r1) => prepend(r1, s)
+      case After(r2) => append(s, r2)
+    }
   }
   case object Naked extends Decoration[Nothing] { def beforeOption = None; def afterOption = None }
   case class Before[R](r: R) extends Decoration[R] { def beforeOption = Some(r); def afterOption = None }
   case class After[R](r: R) extends Decoration[R] { def beforeOption = None; def afterOption = Some(r) }
   case class BeforeAfter[R](before: R, after: R) extends Decoration[R] { def beforeOption = Some(before); def afterOption = Some(after) }
 
-  def empty[R, Ptr[_], Mod <: Mode]: FreeOutput[R, Ptr, Mod] = Empty()
+  implicit def objectOutputInstance[R, Ptr[_]]: ObjectOutput[FreeObjectOutput[R, Ptr], R, Ptr] =
+    new ObjectOutput[FreeObjectOutput[R, Ptr], R, Ptr] {
+      import scalaz.syntax.monad._
 
-  implicit def objectOutputInstance[R, Ptr[_]]: ObjectOutput[FreeOutput[R, Ptr, Append], R, Ptr] =
-    new ObjectOutput[FreeOutput[R, Ptr, Append], R, Ptr] {
-      def write(out: FreeOutput[R, Ptr, Append], r: R): FreeOutput[R, Ptr, Append] = Write(out, r)
-      def writeObject[A](out: FreeOutput[R, Ptr, Append], pa: Ptr[A])(implicit ser: ObjectSerializer[A, R, Ptr]): FreeOutput[R, Ptr, Append] =
-        WriteObject(out, pa, ser)
+      def write(out: FreeObjectOutput[R, Ptr], r: R): FreeObjectOutput[R, Ptr] =
+        new FreeObjectOutput(out.unwrap >> FreeObjectOutput.write(r).unwrap)
+
+      def writeObject[A](out: FreeObjectOutput[R, Ptr], pa: Ptr[A])(implicit ser: ObjectSerializer[A, R, Ptr]): FreeObjectOutput[R, Ptr] =
+        new FreeObjectOutput(out.unwrap >> FreeObjectOutput.writeObject(pa, ser).unwrap)
     }
 }
