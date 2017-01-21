f901acfb9aef818f6c6ccd1512dedd214bc2d811
Tomas Mikula <tomas.mikula@gmail.com>
2017-01-21 11:51:37 -0500

    Add ObjectOutput.shallow

--- src/main/scala/nutcracker/util/ObjectOutput.scala
+++ src/main/scala/nutcracker/util/ObjectOutput.scala
@@ -1,5 +1,7 @@
 package nutcracker.util
 
+import scalaz.~>
+
 /**
   *
   * @tparam O represents output. Examples: String, [[java.io.OutputStream]], ...
@@ -9,4 +11,14 @@ package nutcracker.util
 trait ObjectOutput[O, R, Ptr[_]] {
   def write(out: O, r: R): O
   def writeObject[A](out: O, pa: Ptr[A])(implicit ser: ObjectSerializer[A, R, Ptr]): O
+}
+
+object ObjectOutput {
+
+  def shallow[O, R, Ptr[_]](f: Ptr ~> λ[α => R])(implicit agg: Aggregator[O, R]): ObjectOutput[O, R, Ptr] =
+    new ObjectOutput[O, R, Ptr] {
+      def write(out: O, r: R): O = agg.append(out, r)
+      def writeObject[A](out: O, pa: Ptr[A])(implicit ser: ObjectSerializer[A, R, Ptr]): O = agg.append(out, f(pa))
+    }
+  
 }
