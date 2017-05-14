5c02afc5b8270f1faca5fa1494825683f480fbf7
Tomas Mikula <tomas.mikula@gmail.com>
2017-05-14 17:05:00 -0400

    Minor file renamings.

--- src/main/scala/nutcracker/util/ops/desc.scala
+++ src/main/scala/nutcracker/util/ops/desc.scala
@@ -3,10 +3,12 @@ package nutcracker.util.ops
 import scala.language.implicitConversions
 import nutcracker.util.{DeepShow, Desc}
 
-final case class DescOps[A](a: A) extends AnyVal {
-  def desc[Ptr[_]](implicit ev: DeepShow[A, Ptr]): Desc[Ptr] = ev.free(a)
-}
+object desc extends ToDescOps
 
 trait ToDescOps {
   implicit def toDescOps[A](a: A): DescOps[A] = DescOps(a)
+}
+
+final case class DescOps[A](a: A) extends AnyVal {
+  def desc[Ptr[_]](implicit ev: DeepShow[A, Ptr]): Desc[Ptr] = ev.free(a)
 }
---
+++ src/main/scala/nutcracker/util/ops/lens.scala
@@ -0,0 +1,15 @@
+package nutcracker.util.ops
+
+import scala.language.implicitConversions
+import scalaz.{Lens, Store}
+
+object lens extends ToLensOps
+
+trait ToLensOps {
+  implicit def toLensOps[S](s: S): LensOps[S] = LensOps(s)
+}
+
+final case class LensOps[S](s: S) extends AnyVal {
+  def focus[A](lens: Lens[S, A]): Store[A, S] = lens(s)
+  def set[A](a: A)(implicit lens: Lens[S, A]): S = lens.set(s, a)
+}
