58aeb5dfc8b6e1c5628293a4c4a1f1996f617145
Tomas Mikula <tomas.mikula@gmail.com>
2021-12-04 14:41:21 +0100

    Scala 3 cross-compilation of KMap.

--- src/main/scala/nutcracker/util/KMap.scala
+++ src/main/scala/nutcracker/util/KMap.scala
@@ -24,7 +24,7 @@ final case class KMap[K[_], V[_]](map: Map[K[Any], V[Any]]) extends AnyVal {
       case None => put(k)(v)
       case Some(v0) => put(k)(combineIfPresent(v0, v))
     }
-  def -(k: K[_]): KMap[K, V] = KMap[K, V](map - k.asInstanceOf[K[Any]])
+  def -[A](k: K[A]): KMap[K, V] = KMap[K, V](map - k.asInstanceOf[K[Any]])
   def mapValues[W[_]](f: V ~> W): KMap[K, W] =
     KMap[K, W](map.iterator.map { case (k, v) => (k, f(v.asInstanceOf[V[Any]])) }.toMap[K[Any], W[Any]])
   def ++(that: KMap[K, V]): KMap[K, V] =
@@ -84,7 +84,7 @@ object HHKMap {
 }
 
 /** KMap with an upper bound on the type parameter accepted by K[_], V[_]. */
-final case class KMapB[K[_ <: UB], V[_ <: UB], UB](map: Map[K[_ <: UB], V[_ <: UB]]) extends AnyVal {
+final case class KMapB[K[_ <: UB], V[_ <: UB], UB](map: Map[K[Nothing], V[Nothing]]) extends AnyVal {
   def isEmpty: Boolean = map.isEmpty
   def nonEmpty: Boolean = map.nonEmpty
   def size: Int = map.size
@@ -93,20 +93,20 @@ final case class KMapB[K[_ <: UB], V[_ <: UB], UB](map: Map[K[_ <: UB], V[_ <: U
     BoundedAPair(k, v)
   }
   def tail: KMapB[K, V, UB] = KMapB[K, V, UB](map.tail)
-  def apply[A <: UB](k: K[A]): V[A] = map(k).asInstanceOf[V[A]]
-  def get[A <: UB](k: K[A]): Option[V[A]] = map.get(k).asInstanceOf[Option[V[A]]]
+  def apply[A <: UB](k: K[A]): V[A] = map(k.asInstanceOf[K[Nothing]]).asInstanceOf[V[A]]
+  def get[A <: UB](k: K[A]): Option[V[A]] = map.get(k.asInstanceOf[K[Nothing]]).asInstanceOf[Option[V[A]]]
   def getOrElse[A <: UB](k: K[A])(default: => V[A]): V[A] = get(k).getOrElse(default)
-  def put[A <: UB](k: K[A])(v: V[A]): KMapB[K, V, UB] = KMapB[K, V, UB](map.updated(k, v))
+  def put[A <: UB](k: K[A])(v: V[A]): KMapB[K, V, UB] = KMapB[K, V, UB](map.updated(k.asInstanceOf[K[Nothing]], v.asInstanceOf[V[Nothing]]))
   def updated[A <: UB](k: K[A])(v: V[A])(combineIfPresent: (V[A], V[A]) => V[A]): KMapB[K, V, UB] =
     get(k) match {
       case None => put(k)(v)
       case Some(v0) => put(k)(combineIfPresent(v0, v))
     }
-  def -(k: K[_ <: UB]): KMapB[K, V, UB] = KMapB[K, V, UB](map - k)
+  def -[A <: UB](k: K[A]): KMapB[K, V, UB] = KMapB[K, V, UB](map - k.asInstanceOf[K[Nothing]])
 }
 
 object KMapB {
-  def apply[K[_ <: UB], V[_ <: UB], UB](): KMapB[K, V, UB] = KMapB[K, V, UB](Map[K[_ <: UB], V[_ <: UB]]())
+  def apply[K[_ <: UB], V[_ <: UB], UB](): KMapB[K, V, UB] = KMapB[K, V, UB](Map[K[Nothing], V[Nothing]]())
 }
 
 
@@ -115,69 +115,79 @@ object KMapB {
   * `TC[A, B, C]` being functional in `A`, i.e. for each `A` there are
   * globally unique `B`, `C` such that there is an instance of `TC[A, B, C]`.
   */
-final case class KMap1_2[K[_], V[_, _, _], TC[_, _, _]](map: Map[K[_], V[_, _, _]]) extends AnyVal {
+final case class KMap1_2[K[_], V[_, _, _], TC[_, _, _]](map: Map[K[Nothing], V[Nothing, Nothing, Nothing]]) extends AnyVal {
   def isEmpty: Boolean = map.isEmpty
   def nonEmpty: Boolean = map.nonEmpty
   def size: Int = map.size
   def tail: KMap1_2[K, V, TC] = KMap1_2[K, V, TC](map.tail)
-  def apply[A, B, C](k: K[A])(implicit ev: TC[A, B, C]): V[A, B, C] = map(k).asInstanceOf[V[A, B, C]]
-  def get[A, B, C](k: K[A])(implicit ev: TC[A, B, C]): Option[V[A, B, C]] = map.get(k).asInstanceOf[Option[V[A, B, C]]]
-  def getOrElse[A, B, C](k: K[A])(default: => V[A, _, _])(implicit ev: TC[A, B, C]): V[A, B, C] = get(k).getOrElse(default.asInstanceOf[V[A, B, C]])
-  def put[A, B, C](k: K[A])(v: V[A, B, C])(implicit ev: TC[A, B, C]): KMap1_2[K, V, TC] = KMap1_2[K, V, TC](map.updated(k, v))
+  def apply[A, B, C](k: K[A])(implicit ev: TC[A, B, C]): V[A, B, C] = map(k.asInstanceOf[K[Nothing]]).asInstanceOf[V[A, B, C]]
+  def get[A, B, C](k: K[A])(implicit ev: TC[A, B, C]): Option[V[A, B, C]] = map.get(k.asInstanceOf[K[Nothing]]).asInstanceOf[Option[V[A, B, C]]]
+  def put[A, B, C](k: K[A])(v: V[A, B, C])(implicit ev: TC[A, B, C]): KMap1_2[K, V, TC] =
+    KMap1_2[K, V, TC](map.updated(k.asInstanceOf[K[Nothing]], v.asInstanceOf[V[Nothing, Nothing, Nothing]]))
   def updated[A, B, C](k: K[A])(v: V[A, B, C])(combineIfPresent: (V[A, B, C], V[A, B, C]) => V[A, B, C])(implicit ev: TC[A, B, C]): KMap1_2[K, V, TC] =
     get(k) match {
       case None => put(k)(v)
       case Some(v0) => put(k)(combineIfPresent(v0, v))
     }
-  def -(k: K[_]): KMap1_2[K, V, TC] = KMap1_2[K, V, TC](map - k)
+  def -[A](k: K[A]): KMap1_2[K, V, TC] = KMap1_2[K, V, TC](map - k.asInstanceOf[K[Nothing]])
 }
 
 object KMap1_2 {
-  def apply[K[_], V[_, _, _], TC[_, _, _]](): KMap1_2[K, V, TC] = KMap1_2[K, V, TC](Map[K[_], V[_, _, _]]())
+  def apply[K[_], V[_, _, _], TC[_, _, _]](): KMap1_2[K, V, TC] =
+    KMap1_2[K, V, TC](Map[K[Nothing], V[Nothing, Nothing, Nothing]]())
 }
 
 
-final case class K2Map[K[_, _], V[_, _]](map: Map[K[_, _], V[_, _]]) extends AnyVal {
+final case class K2Map[K[_, _], V[_, _]](map: Map[K[Nothing, Nothing], V[Nothing, Nothing]]) extends AnyVal {
   def isEmpty: Boolean = map.isEmpty
   def nonEmpty: Boolean = map.nonEmpty
   def size: Int = map.size
   def tail: K2Map[K, V] = K2Map[K, V](map.tail)
-  def apply[A, B](k: K[A, B]): V[A, B] = map(k).asInstanceOf[V[A, B]]
-  def get[A, B](k: K[A, B]): Option[V[A, B]] = map.get(k).asInstanceOf[Option[V[A, B]]]
+  def apply[A, B](k: K[A, B]): V[A, B] =
+    map(k.asInstanceOf[K[Nothing, Nothing]]).asInstanceOf[V[A, B]]
+  def get[A, B](k: K[A, B]): Option[V[A, B]] =
+    map.get(k.asInstanceOf[K[Nothing, Nothing]]).asInstanceOf[Option[V[A, B]]]
   def getOrElse[A, B](k: K[A, B])(default: => V[A, B]): V[A, B] = get(k).getOrElse(default)
-  def put[A, B](k: K[A, B])(v: V[A, B]): K2Map[K, V] = K2Map[K, V](map.updated(k, v))
+  def put[A, B](k: K[A, B])(v: V[A, B]): K2Map[K, V] =
+    K2Map[K, V](map.updated(k.asInstanceOf[K[Nothing, Nothing]], v.asInstanceOf[V[Nothing, Nothing]]))
   def updated[A, B](k: K[A, B])(v: V[A, B])(combineIfPresent: (V[A, B], V[A, B]) => V[A, B]): K2Map[K, V] =
     get(k) match {
       case None => put(k)(v)
       case Some(v0) => put(k)(combineIfPresent(v0, v))
     }
-  def -(k: K[_, _]): K2Map[K, V] = K2Map[K, V](map - k)
+  def -[A, B](k: K[A, B]): K2Map[K, V] = K2Map[K, V](map - k.asInstanceOf[K[Nothing, Nothing]])
 }
 
 object K2Map {
-  def apply[K[_, _], V[_, _]](): K2Map[K, V] = K2Map[K, V](Map[K[_, _], V[_, _]]())
+  def apply[K[_, _], V[_, _]](): K2Map[K, V] =
+    K2Map[K, V](Map[K[Nothing, Nothing], V[Nothing, Nothing]]())
 }
 
 
-final case class K3Map[K[_, _, _], V[_, _, _]](map: Map[K[_, _, _], V[_, _, _]]) extends AnyVal {
+final case class K3Map[K[_, _, _], V[_, _, _]](map: Map[K[Nothing, Nothing, Nothing], V[Nothing, Nothing, Nothing]]) extends AnyVal {
   def isEmpty: Boolean = map.isEmpty
   def nonEmpty: Boolean = map.nonEmpty
   def size: Int = map.size
   def tail: K3Map[K, V] = K3Map[K, V](map.tail)
-  def apply[A, B, C](k: K[A, B, C]): V[A, B, C] = map(k).asInstanceOf[V[A, B, C]]
-  def get[A, B, C](k: K[A, B, C]): Option[V[A, B, C]] = map.get(k).asInstanceOf[Option[V[A, B, C]]]
+  def apply[A, B, C](k: K[A, B, C]): V[A, B, C] =
+    map(k.asInstanceOf[K[Nothing, Nothing, Nothing]]).asInstanceOf[V[A, B, C]]
+  def get[A, B, C](k: K[A, B, C]): Option[V[A, B, C]] =
+    map.get(k.asInstanceOf[K[Nothing, Nothing, Nothing]]).asInstanceOf[Option[V[A, B, C]]]
   def getOrElse[A, B, C](k: K[A, B, C])(default: => V[A, B, C]): V[A, B, C] = get(k).getOrElse(default)
-  def put[A, B, C](k: K[A, B, C])(v: V[A, B, C]): K3Map[K, V] = K3Map[K, V](map.updated(k, v))
+  def put[A, B, C](k: K[A, B, C])(v: V[A, B, C]): K3Map[K, V] =
+    K3Map[K, V](map.updated(k.asInstanceOf[K[Nothing, Nothing, Nothing]], v.asInstanceOf[V[Nothing, Nothing, Nothing]]))
   def updated[A, B, C](k: K[A, B, C])(v: V[A, B, C])(combineIfPresent: (V[A, B, C], V[A, B, C]) => V[A, B, C]): K3Map[K, V] =
     get(k) match {
       case None => put(k)(v)
       case Some(v0) => put(k)(combineIfPresent(v0, v))
     }
-  def -(k: K[_, _, _]): K3Map[K, V] = K3Map[K, V](map - k)
+  def -[A, B, C](k: K[A, B, C]): K3Map[K, V] =
+    K3Map[K, V](map - k.asInstanceOf[K[Nothing, Nothing, Nothing]])
 }
 
 object K3Map {
-  def apply[K[_, _, _], V[_, _, _]](): K3Map[K, V] = K3Map[K, V](Map[K[_, _, _], V[_, _, _]]())
+  def apply[K[_, _, _], V[_, _, _]](): K3Map[K, V] =
+    K3Map[K, V](Map[K[Nothing, Nothing, Nothing], V[Nothing, Nothing, Nothing]]())
 }
 
 sealed abstract class K3Pair[K[_, _, _], V[_, _, _]] {
